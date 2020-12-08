98e4b0a46c236c0c26564854275de694d510fd87
MacFan4000 <paulfkeffer@gmail.com>
2020-12-08 09:54:10 -0500

    Update PhabricatorMediaWikiAuthProvider.php

--- src/oauth/PhabricatorMediaWikiAuthProvider.php
+++ src/oauth/PhabricatorMediaWikiAuthProvider.php
@@ -1,49 +1,35 @@
 <?php
 
 final class PhabricatorMediaWikiAuthProvider
-  extends PhabricatorOAuth1AuthProvider {
+  extends PhabricatorOAuthAuthProvider {
 
-  const PROPERTY_MEDIAWIKI_NAME = 'oauth1:mediawiki:name';
-  const PROPERTY_MEDIAWIKI_URI = 'oauth1:mediawiki:uri';
-  const PROPERTY_PRIVATE_KEY = 'oauth1:mediawiki:key:private';
-  const PROPERTY_PUBLIC_KEY = 'oauth1:mediawiki:key:public';
+  const PROPERTY_MEDIAWIKI_NAME = 'oauth2:mediawiki:name';
+  const PROPERTY_MEDIAWIKI_URI = 'oauth2:mediawiki:uri';
 
-  public function getProviderConfig() {
-      $config = parent::getProviderConfig();
-      $config->setProviderType('mediawiki');
-      return $config;
-  }
-
-  public function readFormValuesFromProvider() {
+ public function readFormValuesFromProvider() {
     $config = $this->getProviderConfig();
-    return array(
-      self::PROPERTY_MEDIAWIKI_NAME =>
-        $this->getProviderDomain(),
-      self::PROPERTY_MEDIAWIKI_URI =>
-        $config->getProperty(self::PROPERTY_MEDIAWIKI_URI),
-      self::PROPERTY_CONSUMER_KEY =>
-        $config->getProperty(self::PROPERTY_CONSUMER_KEY),
-      self::PROPERTY_CONSUMER_SECRET =>
-        $config->getProperty(self::PROPERTY_CONSUMER_SECRET),
+    $uri = $config->getProperty(self::PROPERTY_MEDIAWIKI_URI);
+
+    return parent::readFormValuesFromProvider() + array(
+      self::PROPERTY_MEDIAWIKI_NAME => $this->getProviderDomain(),
+      self::PROPERTY_MEDIAWIKI_URI  => $uri,
     );
   }
 
   public function readFormValuesFromRequest(AphrontRequest $request) {
-    $is_setup = $this->isSetup();
+    $is_setup = $this->isCreate();
     if ($is_setup) {
+      $parent_values = array();
       $name = $request->getStr(self::PROPERTY_MEDIAWIKI_NAME);
     } else {
+      $parent_values = parent::readFormValuesFromRequest($request);
       $name = $this->getProviderDomain();
     }
 
-    return array(
+    return $parent_values + array(
       self::PROPERTY_MEDIAWIKI_NAME => $name,
       self::PROPERTY_MEDIAWIKI_URI =>
         $request->getStr(self::PROPERTY_MEDIAWIKI_URI),
-      self::PROPERTY_CONSUMER_KEY =>
-        $request->getStr(self::PROPERTY_CONSUMER_KEY),
-      self::PROPERTY_CONSUMER_SECRET =>
-        $request->getStr(self::PROPERTY_CONSUMER_SECRET),
     );
   }
 
@@ -67,16 +53,24 @@ final class PhabricatorMediaWikiAuthProvider
     }
     return $normalized;
   }
-
-  protected function getProviderConfigurationHelp() {
-    $login_uri = PhabricatorEnv::getURI($this->getLoginURI());
-    if ($this->isSetup()) {
+  
+  public function getConfigurationHelp() {
+    if ($this->isCreate()) {
       return pht(
         "**Step 1 of 2**: Provide the name and URI for your MediaWiki install.\n\n".
-        "In the next step, you will create an auth consumer in MediaWiki to be used by Phabricator oauth.");
-    } else {
-      $wiki_uri = $this->getWikiURI();
-      return pht(
+        "In the next step, you will create an auth consumer in MediaWiki to be used by Phabricator oauth.")
+    }
+
+    return parent::getConfigurationHelp();
+  }
+
+  protected function getProviderConfigurationHelp() {
+    $config = $this->getProviderConfig();
+    $base_uri = rtrim(
+      $config->getProperty(self::PROPERTY_MEDIAWIKI_URI), '/');
+    $login_uri = PhabricatorEnv::getURI($this->getLoginURI());
+
+    return pht(
         "**Step 2 of 2**: Create a MediaWiki auth consumer for this Phabricator instance.".
         "\n\n".
         "NOTE: Propose a consumer with the form at this url: %s".
@@ -96,7 +90,6 @@ final class PhabricatorMediaWikiAuthProvider
         $wiki_uri.'/index.php?title=Special:OAuthConsumerRegistration/propose',
         $login_uri,
         $wiki_uri.'/index.php?title=Special:OAuthManageConsumers/proposed');
-    }
   }
 
   protected function newOAuthAdapter() {
@@ -104,33 +97,44 @@ final class PhabricatorMediaWikiAuthProvider
 
     return id(new PhutilMediaWikiAuthAdapter())
       ->setAdapterDomain($config->getProviderDomain())
-      ->setMediaWikiBaseURI($this->getWikiURI());
+      ->setPhabricatorBaseURI(
+        $config->getProperty(self::PROPERTY_MEDIAWIKI_URI));
   }
 
   protected function getLoginIcon() {
     return 'MediaWiki';
   }
 
-  private function isSetup() {
+  private function isCreate() {
     return !$this->getProviderConfig()->getID();
-  }
-
-  public function hasSetupStep() {
-    return true;
   }
 
   public function processEditForm(
     AphrontRequest $request,
     array $values) {
-    $errors = array();
-    $issues = array();
 
-    $is_setup = $this->isSetup();
+    $is_setup = $this->isCreate();
+
+    if (!$is_setup) {
+      list($errors, $issues, $values) =
+        parent::processEditForm($request, $values);
+    } else {
+      $errors = array();
+      $issues = array();
+    }
 
     $key_name = self::PROPERTY_MEDIAWIKI_NAME;
     $key_uri = self::PROPERTY_MEDIAWIKI_URI;
-    $key_secret = self::PROPERTY_CONSUMER_SECRET;
-    $key_consumer = self::PROPERTY_CONSUMER_KEY;
+
+    if (!strlen($values[$key_name])) {
+      $errors[] = pht('MediaWiki instance name is required.');
+      $issues[$key_name] = pht('Required');
+    } else if (!preg_match('/^[a-z0-9.]+\z/', $values[$key_name])) {
+      $errors[] = pht(
+        'MediaWiki instance name must contain only lowercase letters, '.
+        'digits, and periods.');
+      $issues[$key_name] = pht('Invalid');
+    }
 
     if (!strlen($values[$key_uri])) {
       $errors[] = pht('MediaWiki base URI is required.');
@@ -139,37 +143,18 @@ final class PhabricatorMediaWikiAuthProvider
       $uri = new PhutilURI($values[$key_uri]);
       if (!$uri->getProtocol()) {
         $errors[] = pht(
-          'MediaWiki base URI should include protocol '
-         .'(like "https://").');
+          'MediaWiki base URI should include protocol (like "%s").',
+          'https://');
         $issues[$key_uri] = pht('Invalid');
       }
     }
 
-    if (!$is_setup && !strlen($values[$key_secret])) {
-      $errors[] = pht('Consumer Secret is required');
-      $issues[$key_secret] = pht('Required');
-    }
-
-    if (!$is_setup && !strlen($values[$key_consumer])) {
-      $errors[] = pht('Consumer Key is required');
-      $issues[$key_consumer] = pht('Required');
-    }
-
-    if (!count($errors)) {
+    if (!$errors && $is_setup) {
       $config = $this->getProviderConfig();
+
       $config->setProviderDomain($values[$key_name]);
-      $config->setProperty($key_name, $values[$key_name]);
-      if ($is_setup) {
-
-
-        $config->setProperty($key_uri, $values[$key_uri]);
-      } else {
-        $config->setProperty($key_uri, $values[$key_uri]);
-        $config->setProperty($key_secret, $values[$key_secret]);
-        $config->setProperty($key_consumer, $values[$key_consumer]);
-      }
-      $config->save();
     }
+
     return array($errors, $issues, $values);
   }
 
@@ -179,7 +164,7 @@ final class PhabricatorMediaWikiAuthProvider
     array $values,
     array $issues) {
 
-    $is_setup = $this->isSetup();
+    $is_setup = $this->isCreate();
 
     $e_required = $request->isFormPost() ? null : true;
 
@@ -193,34 +178,22 @@ final class PhabricatorMediaWikiAuthProvider
     $v_uri = $values[self::PROPERTY_MEDIAWIKI_URI];
     $e_uri = idx($issues, self::PROPERTY_MEDIAWIKI_URI, $e_required);
 
-    $config = $this->getProviderConfig();
-
     if ($is_setup) {
       $form
-        ->appendRemarkupInstructions(
-          pht(
-            "**MediaWiki Instance Name**\n\n".
-            "Choose a permanent name for this instance of MediaWiki.".
-            "Phabricator uses this name internally to keep track of ".
-            "this instance of MediaWiki, in case the URL changes later.".
-            "\n\n".
-            "Use lowercase letters, digits, and period. For example: ".
-            "\n\n`mediawiki`, `mediawiki.mycompany` ".
-            "or `mediawiki.engineering` are reasonable names."))
-        ->appendChild(
+       ->appendChild(
           id(new AphrontFormTextControl())
             ->setLabel(pht('MediaWiki Instance Name'))
             ->setValue($v_name)
             ->setName(self::PROPERTY_MEDIAWIKI_NAME)
-            ->setError($e_name));
+            ->setError($e_name)
+            ->setCaption(pht(
+            'Use lowercase letters, digits, and periods. For example: %s',)));
     } else {
-      $form->appendChild(
-          id(new AphrontFormTextControl())
+      $form
+        ->appendChild(
+          id(new AphrontFormStaticControl())
             ->setLabel(pht('MediaWiki Instance Name'))
-            ->setValue($v_name)
-            ->setName(self::PROPERTY_MEDIAWIKI_NAME)
-            ->setDisabled(true)
-            ->setError($e_name));
+            ->setValue($v_name));
     }
 
     $form
@@ -228,31 +201,18 @@ final class PhabricatorMediaWikiAuthProvider
         id(new AphrontFormTextControl())
           ->setLabel(pht('MediaWiki Base URI'))
           ->setValue($v_uri)
-          ->setName(self::PROPERTY_MEDIAWIKI_URI)
-          ->setPlaceholder('https://www.mediawiki.org/w')
-          ->setCaption(pht('The full URL to your MediaWiki innstall, up to but not including "index.php"'))
+          ->setName(self::PROPERTY_PHABRICATOR_URI)
+          ->setCaption(
+            pht('URL to your MediaWiki install upto but not including index.php',))
           ->setError($e_uri));
 
     if (!$is_setup) {
-      if (!strlen($config->getProperty(self::PROPERTY_CONSUMER_KEY))) {
-        $form->appendRemarkupInstructions(
-          pht(
-            'NOTE: Copy the keys generated by the MediaWiki OAuth'.
-            ' consumer registration and paste them here.'));
-      }
-
-      $form
-      ->appendChild(
-        id(new AphrontFormTextControl())
-          ->setLabel(pht('Consumer Key'))
-          ->setName(self::PROPERTY_CONSUMER_KEY)
-          ->setValue($values[self::PROPERTY_CONSUMER_KEY]))
-      ->appendChild(
-        id(new AphrontFormTextControl())
-          ->setLabel(pht('Secret Key'))
-          ->setName(self::PROPERTY_CONSUMER_SECRET)
-          ->setValue($values[self::PROPERTY_CONSUMER_SECRET]));
+      parent::extendEditForm($request, $form, $values, $issues);
     }
+  }
+
+  public function hasSetupStep() {
+    return true;
   }
 
   public static function getMediaWikiProvider() {
