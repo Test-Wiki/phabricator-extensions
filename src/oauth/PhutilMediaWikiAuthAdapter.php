4008684640088153e052b33fc94bab6e561c0273
MacFan4000 <paulfkeffer@gmail.com>
2020-12-17 15:42:08 -0500

    Update PhutilMediaWikiAuthAdapter.php

--- src/oauth/PhutilMediaWikiAuthAdapter.php
+++ src/oauth/PhutilMediaWikiAuthAdapter.php
@@ -81,6 +81,10 @@ final class PhutilMediaWikiAuthAdapter
     return $this->getMediaWikiURI('rest.php/oauth2/access_token');
   }
     
+  public function getScope() {
+     return '';
+   }
+    
   public function getExtraAuthenticateParameters() {
     return array(
       'response_type' => 'code',
